06b15b8d3875ec41c64fcd2073d97d879b1d7c03
Joe Audette <joeaudette@users.noreply.github.com>
2016-01-03 14:34:32 -0500

    data updates should use CancellationToken.None

--- src/cloudscribe.Logging.Web/LogManager.cs
+++ src/cloudscribe.Logging.Web/LogManager.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 //	Author:                 Joe Audette
 //  Created:			    2015-12-23
-//	Last Modified:		    2015-12-27
+//	Last Modified:		    2016-01-03
 // 
 
 using System.Collections.Generic;
@@ -45,12 +45,12 @@ namespace cloudscribe.Logging.Web
 
         public async Task<bool> DeleteLogItem(int id)
         {
-            return await logRepo.Delete(id, CancellationToken);
+            return await logRepo.Delete(id, CancellationToken.None);
         }
 
         public async Task<bool> DeleteAllLogItems()
         {
-            return await logRepo.DeleteAll(CancellationToken);
+            return await logRepo.DeleteAll(CancellationToken.None);
         }
 
     }
