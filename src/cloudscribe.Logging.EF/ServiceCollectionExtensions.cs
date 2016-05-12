47e8a964428f80230ee56cad9034fdd2df44942d
Joe Audette <joeaudette@users.noreply.github.com>
2016-05-12 13:06:46 -0400

    refactoring

---
+++ src/cloudscribe.Logging.EF/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using cloudscribe.Logging.EF;
+using cloudscribe.Logging.Web;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCloudscribeLoggingEFStorage(
+            this IServiceCollection services,
+            string connectionString
+            )
+        {
+            services.AddEntityFramework()
+                        .AddSqlServer()
+                        .AddDbContext<LoggingDbContext>(options =>
+                        {
+                            options.UseSqlServer(connectionString);
+                        });
+
+            services.TryAddScoped<ILogModelMapper, SqlServerLogModelMapper>();
+            services.AddScoped<ILogRepository, LogRepository>();
+
+            return services;
+        }
+    }
+}
