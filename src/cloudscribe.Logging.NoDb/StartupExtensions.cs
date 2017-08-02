1e740d4ff592ca61395d6970d1eea5e4655079fe
Joe Audette <joe.audette@gmail.com>
2017-08-02 12:54:20 -0400

    IConfigurationRoot to IConfiguration

--- src/cloudscribe.Logging.NoDb/StartupExtensions.cs
+++ src/cloudscribe.Logging.NoDb/StartupExtensions.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Author:					Joe Audette
 // Created:					2016-06-25
-// Last Modified:			2016-07-02
+// Last Modified:			2017-08-02
 // 
 
 
@@ -18,7 +18,7 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class StartupExtensions
     {
 
-        public static IServiceCollection AddCloudscribeLoggingNoDbStorage(this IServiceCollection services, IConfigurationRoot configuration)
+        public static IServiceCollection AddCloudscribeLoggingNoDbStorage(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<NoDbLogOptions>(configuration.GetSection("NoDbLogOptions"));
             services.TryAddScoped<IStoragePathResolver<LogItem>, LogItemStoragePathResolver>();
