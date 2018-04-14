d20adbfe5d11ba5acc6c2b3e3295b0c0446d89ad
Joe Audette <joe.audette@gmail.com>
2018-04-14 11:52:13 -0400

    remove web deps from data layer

--- src/cloudscribe.Logging.EFCore.MySql/Program.cs
+++ src/cloudscribe.Logging.EFCore.MySql/Program.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-
-namespace cloudscribe.Logging.EFCore.MySql
+﻿namespace cloudscribe.Logging.EFCore.MySql
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-		    var host = new WebHostBuilder()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                //.UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+		    
         }
     }
 }
